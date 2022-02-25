bdba66f0ffb80e93c9e27d2f613a3a6e397499c5
Luiz Filipe da Silva <luiz.silva@itmapps.com.br>
2022-02-25 17:26:01 -0300

    Create validators

---
+++ src/validators/is-foreign-key.ts
@@ -0,0 +1,47 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments,
+} from 'class-validator';
+import { getRepository } from 'typeorm';
+
+@ValidatorConstraint({
+  async: true,
+})
+export class IsForeignKeyConstraint implements ValidatorConstraintInterface {
+
+  async validate(id: any, args: ValidationArguments): Promise<boolean> {
+    const entity = args.constraints[0];
+    const repository = getRepository(entity);
+    const record = await repository.findOne(id);
+
+    return !!record;
+  }
+
+  defaultMessage(validationArguments?: ValidationArguments): string {
+    return 'A chave estrangeira ($value) não existe.'
+  }
+
+}
+
+/**
+ * Validates the property value as an existing foreign key.
+ * 
+ * @param {any} entity Entity to which the foreign key belongs.
+ * @param {ValidationOptions} validationOptions Validation options.
+ * 
+ * @returns Property decorator.
+ */
+export function IsForeignKey(entity: any, validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [entity],
+      validator: IsForeignKeyConstraint,
+    });
+  };
+}
---
+++ src/validators/is-new.ts
@@ -0,0 +1,47 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments,
+} from 'class-validator';
+import { getRepository } from 'typeorm';
+
+@ValidatorConstraint({
+  async: true,
+})
+export class IsNewConstraint implements ValidatorConstraintInterface {
+
+  async validate(value: any, args: ValidationArguments): Promise<boolean> {
+    const [entity, propertyName] = args.constraints;
+    const repository = getRepository(entity);
+    const record = await repository.findOne({ [propertyName]: value });
+
+    return !record;
+  }
+
+  defaultMessage(validationArguments?: ValidationArguments): string {
+    return `O valor '$value' já foi utilizado em outro registro e não pode ser repetido.`;
+  }
+
+}
+
+/**
+ * Validates the property value as an existing foreign key.
+ * 
+ * @param {any} entity Entity to which the foreign key belongs.
+ * @param {ValidationOptions} validationOptions Validation options.
+ * 
+ * @returns Property decorator.
+ */
+export function IsNew(entity?: any, validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [entity || object.constructor, propertyName],
+      validator: IsNewConstraint,
+    });
+  };
+}
